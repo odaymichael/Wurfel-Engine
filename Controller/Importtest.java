789ae3ef172234a7a17f98fcbaac4c961ecfdb61
Benedikt Vogler <e-mail@benediktvogler.de>
2012-04-29 15:13:23 +0200

    Better chunks
    Smoother scrolling and the lines are correct with the tiles.

---
+++ Controller/Importtest.java
@@ -0,0 +1,13 @@
+package Controller;
+
+public class Importtest {
+    public String name;  
+    
+    public Importtest(){
+        this.name = "Penis";
+    }
+    
+    public String returnfunction(){
+        return this.name;
+    }
+}
