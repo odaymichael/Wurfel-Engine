477ae22a62358e5480ab63ddeddaf3148dca863e
Benedikt Vogler <e-mail@benediktvogler.de>
2012-12-13 21:06:50 +0100

    Added class for Explosive Barrel

---
+++ com/BombingGames/Game/Blocks/ExplosiveBarrel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.BombingGames.Game.Blocks;
+
+/**
+ *A barrel block which can explode
+ * @author Benedikt
+ */
+public class ExplosiveBarrel extends SelfAwareBlock {
+    
+    
+}
