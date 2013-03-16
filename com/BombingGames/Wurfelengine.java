146a3176e9049f49220c22e35e16f531b14d005a
Benedikt Vogler <e-mail@benediktvogler.de>
2013-03-16 20:24:20 +0100

    Version coutner increment

--- com/BombingGames/Wurfelengine.java
+++ com/BombingGames/Wurfelengine.java
@@ -18,7 +18,7 @@ public class Wurfelengine extends StateBasedGame {
     /**
      * The Version of the Engine
      */
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "0.6";
     
     private static AppGameContainer game;    
     private static File workingDirectory;
