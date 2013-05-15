3adfb689c4013868d83fa8bb29550b64330d0172
Benedikt Vogler <e-mail@benediktvogler.de>
2013-05-15 23:16:54 +0200

    Added empty CustomGameView

---
+++ com/BombingGames/Game/CustomGame/CustomGameView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.BombingGames.Game.CustomGame;
+
+import com.BombingGames.Game.Controller;
+import com.BombingGames.Game.View;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+
+/**
+ *
+ * @author Benedikt
+ */
+public class CustomGameView extends View{
+
+    public CustomGameView(GameContainer gc, Controller controller) throws SlickException {
+        super(gc, controller);
+    }
+    
+    
+    
+}
