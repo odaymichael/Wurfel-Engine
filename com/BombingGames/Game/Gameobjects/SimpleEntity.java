517a705a410b2f9d565c19ef4b8dc057122f6c1e
Benedikt Vogler <e-mail@benediktvogler.de>
2013-05-15 23:16:38 +0200

    Added a simple Entity for simple images

---
+++ com/BombingGames/Game/Gameobjects/SimpleEntity.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.BombingGames.Game.Gameobjects;
+
+/**
+ *
+ * @author Benedikt
+ */
+class SimpleEntity extends AbstractEntity {
+
+    public SimpleEntity(int id) {
+        super(id);
+    }
+
+    @Override
+    public void update(int delta) {
+    }
+    
+}
