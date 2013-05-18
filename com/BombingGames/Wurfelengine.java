6dd8914b3561e016c06e07f73e3f4fd42e32e4e7
Benedikt Vogler <e-mail@benediktvogler.de>
2013-05-18 15:51:21 +0200

    Version update

--- com/BombingGames/Wurfelengine.java
+++ com/BombingGames/Wurfelengine.java
@@ -13,7 +13,7 @@ public class Wurfelengine extends AppGameContainer {
     /**
      * The Version of the Engine
      */
-    public static final String VERSION = "1.0.1";    
+    public static final String VERSION = "1.0.2";    
     private static File workingDirectory;
 
     /**
