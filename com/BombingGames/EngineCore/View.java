odaymichael/Wurfel-Engine

commit 8abd0ffb26d57e2d19f8cbed260de24bcf6b74a1

Big refactoring

com/BombingGames/EngineCore/View.java
@@ -161,16 +161,16 @@ public class View {
         
         //find out where the click went
         Coordinate coords = new Coordinate(
-                       x / Block.DIMENSION -1,
-                       2*y / Block.DIMENSION -1,
+                       x / Block.SCREEN_WIDTH -1,
+                       2*y / Block.SCREEN_DEPTH -1,
                        Map.getBlocksZ()-1,
                         true
         );
        
         //find the specific coordinate
-        Coordinate specificCoords = AbstractGameObject.sideIDtoNeighbourCoords(
+        Coordinate specificCoords = Coordinate.neighbourSidetoCoords(
                             coords,
-                            AbstractGameObject.getSideID(x % Block.DIMENSION, y % Block.DIMENSION)
+                            Coordinate.getNeighbourSide(x % Block.SCREEN_DEPTH, y % Block.SCREEN_DEPTH)
         );
         coords.setRelX(specificCoords.getRelX());
         coords.setRelY(specificCoords.getRelY() + coords.getZ()*2);

com/BombingGames/EngineCore/Controller.java
@@ -255,7 +255,7 @@ public class Controller {
      * @return the neighbour block
      */
     public static Block getNeighbourBlock(Coordinate coords, int side){
-        return Controller.getMapDataSafe(Block.sideIDtoNeighbourCoords(coords, side));
+        return Controller.getMapDataSafe(Coordinate.neighbourSidetoCoords(coords, side));
     }
     
   

com/BombingGames/EngineCore/Map/Cell.java
@@ -10,39 +10,78 @@ public class Cell {
     private Block block;
     private float[] cellOffset;
 
+    /**
+     *Create a cell containing air-
+     */
     public Cell() {
         block = Block.getInstance(0);
-        cellOffset = new float[]{Block.DIM2, Block.DIM2,0};
+        cellOffset = new float[]{0, 0,0};
     }
     
+    /**
+     *
+     * @param id
+     */
     public void newBlock(int id){
         this.block = Block.getInstance(id);
     }
     
+    /**
+     *
+     * @param id
+     * @param value
+     */
     public void newBlock(int id, int value){
        this.block = Block.getInstance(id, value); 
     }
     
+    /**
+     *
+     * @param id
+     * @param value
+     * @param coords
+     */
     public void newBlock(int id, int value, Coordinate coords){
        this.block = Block.getInstance(id, value, coords); 
     }
 
+    /**
+     *
+     * @return
+     */
     public Block getBlock() {
         return block;
     }
 
+    /**
+     *
+     * @param block
+     */
     public void setBlock(Block block) {
         this.block = block;
     }
 
+    /**
+     *The cell offset has it's center in the top left corner.
+     * @return
+     */
     public float[] getCellOffset() {
         return cellOffset;
     }
 
+    /**
+     *The cell offset has it's center in the top left corner.
+     * @param cellOffset
+     */
     public void setCellOffset(float[] cellOffset) {
         this.cellOffset = cellOffset;
     }
 
+    /**
+     *
+     * @param field
+     * @param offset
+     */
     public void setCellOffset(int field, float offset) {
         this.cellOffset[field] = offset;
     }

com/BombingGames/EngineCore/Map/Coordinate.java
@@ -1,11 +1,8 @@
 package com.BombingGames.EngineCore.Map;
 
 import com.BombingGames.EngineCore.Controller;
-import static com.BombingGames.EngineCore.Map.Coordinate.getMapCenter;
+import com.BombingGames.Game.Gameobjects.AbstractGameObject;
 import com.BombingGames.Game.Gameobjects.Block;
-import static com.BombingGames.Game.Gameobjects.AbstractGameObject.DIM2;
-import static com.BombingGames.Game.Gameobjects.AbstractGameObject.DIM4;
-import static com.BombingGames.Game.Gameobjects.AbstractGameObject.DIMENSION;
 
 /**
  *A coordinate is a reference to a specific cell in the map. The coordinate can transfer between relative and absolute coordiantes.
@@ -148,7 +145,7 @@ public class Coordinate {
     }
     
     /**
-     * Geht the height (z-value) of the coordinate.
+     * Geht the height (z-value) of the coordinate (game dimension).
      * @return
      */
     public float getHeight(){
@@ -300,7 +297,7 @@ public class Coordinate {
      * @return when it has offset true, else false
      */
     public boolean hasOffset() {
-        return getCellOffset()[0] != DIM2 || getCellOffset()[1] != DIM2 || getCellOffset()[2] != 0;
+        return getCellOffset()[0] != 0 || getCellOffset()[1] != 0 || getCellOffset()[2] != 0;
     }
     
    /**
@@ -316,8 +313,8 @@ public class Coordinate {
      * @return
      */
     public int get2DPosX() {
-        return getRelX() * DIMENSION //x-coordinate multiplied by it's dimension in this direction
-               + (getRelY() % 2) * DIM2; //y-coordinate multiplied by it's dimension in this direction
+        return getRelX() * Block.SCREEN_WIDTH //x-coordinate multiplied by it's dimension in this direction
+               + (getRelY() % 2) * AbstractGameObject.SCREEN_WIDTH2; //y-coordinate multiplied by it's dimension in this direction
     }
     
     /**
@@ -325,7 +322,7 @@ public class Coordinate {
      * @return
      */
     public int get2DPosY() {
-        return getRelY() * DIM4 //x-coordinate * the tile's size
+        return getRelY() * Block.SCREEN_DEPTH2 //y-coordinate * the tile's half size size
                - (int) (getHeight() / Math.sqrt(2)); //take axis shortening into account
     }
     
@@ -341,5 +338,102 @@ public class Coordinate {
     public boolean onLoadedMap(){
         return (getRelX() >= 0 && getRelX() < Map.getBlocksX()
             && getRelY() >= 0 && getRelY() < Map.getBlocksY());
+    }
+    
+    /**
+     * Returns the field-id where the coordiantes are inside in relation to the current field. Field id count clockwise, starting with the top with 0.
+     * If you want to get the neighbour you can use neighbourSidetoCoords(Coordinate coords, int sideID) with the second parameter found by this function.
+     * The numbering of the sides:<br>
+     * 7 \ 0 / 1<br>
+     * -------<br>
+     * 6 | 8 | 2<br>
+     * -------<br>
+     * 5 / 4 \ 3<br>
+     * @param x game-space-coordinates, value in pixels
+     * @param y game-space-coordinates, value in pixels
+     * @return Returns the fieldnumber of the coordinates. 8 is the field itself.
+     * @see com.BombingGames.Game.Gameobjects.AbstractGameObject#neighbourSidetoCoords(com.BombingGames.EngineCore.Map.Coordinate, int)
+     */
+    public static int getNeighbourSide(float x, float y) {
+        x += Block.SCREEN_WIDTH2;
+        y += Block.SCREEN_DEPTH2;
+        
+        int result = 8;//standard result
+        if (x + y <= Block.SCREEN_DEPTH) {
+            result = 7;
+        }
+        if (x - y >= Block.SCREEN_DEPTH) {
+            if (result == 7) {
+                result = 0;
+            } else {
+                result = 1;
+            }
+        }
+        if (x + y >= 3 * Block.SCREEN_DEPTH) {
+            if (result == 1) {
+                result = 2;
+            } else {
+                result = 3;
+            }
+        }
+        if (-x + y >= Block.SCREEN_DEPTH) {
+            if (result == 3) {
+                result = 4;
+            } else if (result == 7) {
+                result = 6;
+            } else {
+                result = 5;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Get the neighbour coordinates of the neighbour of the coords you give.
+     * @param coords the coordinates of the field
+     * @param neighbourSide the side number of the given coordinates
+     * @return The coordinates of the neighbour.
+     */
+    public static Coordinate neighbourSidetoCoords(Coordinate coords, int neighbourSide) {
+        int[] result = new int[3];
+        switch (neighbourSide) {
+            case 0:
+                result[0] = coords.getRelX();
+                result[1] = coords.getRelY() - 2;
+                break;
+            case 1:
+                result[0] = coords.getRelX() + (coords.getRelY() % 2 == 1 ? 1 : 0);
+                result[1] = coords.getRelY() - 1;
+                break;
+            case 2:
+                result[0] = coords.getRelX() + 1;
+                result[1] = coords.getRelY();
+                break;
+            case 3:
+                result[0] = coords.getRelX() + (coords.getRelY() % 2 == 1 ? 1 : 0);
+                result[1] = coords.getRelY() + 1;
+                break;
+            case 4:
+                result[0] = coords.getRelX();
+                result[1] = coords.getRelY() + 2;
+                break;
+            case 5:
+                result[0] = coords.getRelX() - (coords.getRelY() % 2 == 0 ? 1 : 0);
+                result[1] = coords.getRelY() + 1;
+                break;
+            case 6:
+                result[0] = coords.getRelX() - 1;
+                result[1] = coords.getRelY();
+                break;
+            case 7:
+                result[0] = coords.getRelX() - (coords.getRelY() % 2 == 0 ? 1 : 0);
+                result[1] = coords.getRelY() - 1;
+                break;
+            default:
+                result[0] = coords.getRelX();
+                result[1] = coords.getRelY();
+        }
+        result[2] = coords.getZ();
+        return new Coordinate(result[0], result[1], result[2], true);
     }
 }

com/BombingGames/EngineCore/Map/Map.java
@@ -416,8 +416,8 @@ public class Map {
         }
         
         for (int i=0;i < numberofblocks; i++){
-                //cellPos[x[i]][y[i]][z[i]][0] = (float) (Math.random()*Block.DIM2);
-                //cellPos[x[i]][y[i]][z[i]][1] = (float) (Math.random()*Block.DIM2);
+                //cellPos[x[i]][y[i]][z[i]][0] = (float) (Math.random()*Block.SCREEN_DEPTH2);
+                //cellPos[x[i]][y[i]][z[i]][1] = (float) (Math.random()*Block.SCREEN_DEPTH2);
                 data[x[i]][y[i]][z[i]].setCellOffset(2, (float) (Math.random()*Block.GAMEDIMENSION));//vertical shake
             
         }

com/BombingGames/EngineCore/Map/Minimap.java
@@ -127,43 +127,43 @@ public class Minimap {
             shapeRenderer.begin(ShapeType.Rectangle);
             shapeRenderer.setColor(Color.GREEN);
             shapeRenderer.rect(
-                viewportPosX + scaleX * camera.getGamePosX() / Block.DIMENSION,
-                viewportPosY + scaleY * camera.getGamePosY() / (Block.DIM2/2),
-                scaleX*camera.get2DWidth() / Block.DIMENSION,
-                scaleY*camera.get2DHeight() / (Block.DIM2/2)
+                viewportPosX + scaleX * camera.getGamePosX() / Block.SCREEN_WIDTH,
+                viewportPosY + scaleY * camera.getGamePosY() / Block.SCREEN_DEPTH4,
+                scaleX*camera.get2DWidth() / Block.SCREEN_DEPTH,
+                scaleY*camera.get2DHeight() / Block.SCREEN_DEPTH4
             );
 
             if (controller.getPlayer()!=null){
                 //player level getCameras() rectangle
                 shapeRenderer.setColor(Color.GRAY);
                 shapeRenderer.rect(
-                    viewportPosX + scaleX * camera.getGamePosX() / Block.DIMENSION,
-                    viewportPosY + scaleY * camera.getGamePosY() / (Block.DIM2/2)
-                    + scaleY *2*(controller.getPlayer().getCoords().getZ() * (Block.DIM2/2))/ (float) (Block.DIMENSION),
-                    scaleX*camera.get2DWidth() / Block.DIMENSION,
-                    scaleY*camera.get2DHeight() / (Block.DIM2/2)
+                    viewportPosX + scaleX * camera.getGamePosX() / Block.SCREEN_WIDTH,
+                    viewportPosY + scaleY * camera.getGamePosY() / Block.SCREEN_DEPTH4
+                    + scaleY *2*(controller.getPlayer().getCoords().getZ() * Block.SCREEN_HEIGHT2)/ (float) (Block.SCREEN_DEPTH),
+                    scaleX*camera.get2DWidth() / Block.SCREEN_DEPTH,
+                    scaleY*camera.get2DHeight() / Block.SCREEN_DEPTH4
                 );
             }
 
             //top level getCameras() rectangle
             shapeRenderer.setColor(Color.WHITE);
             shapeRenderer.rect(
-                viewportPosX + scaleX * camera.getGamePosX() / Block.DIMENSION,
-                viewportPosY + scaleY * camera.getGamePosY() / (Block.DIM2/2)
-                + scaleY *2*(Chunk.getBlocksZ() * Block.DIM2)/ (float) (Block.DIMENSION),
-                scaleX*camera.get2DWidth() / Block.DIMENSION,
-                scaleY*camera.get2DHeight() / (Block.DIM2/2)
+                viewportPosX + scaleX * camera.getGamePosX() / Block.SCREEN_WIDTH,
+                viewportPosY + scaleY * camera.getGamePosY() / Block.SCREEN_DEPTH4
+                + scaleY *2*(Chunk.getBlocksZ() * Block.SCREEN_DEPTH2)/ (float) (Block.SCREEN_DEPTH),
+                scaleX*camera.get2DWidth() / Block.SCREEN_DEPTH,
+                scaleY*camera.get2DHeight() / Block.SCREEN_DEPTH4
             );
             shapeRenderer.end();
             
             if (controller.getPlayer()!=null){
                 view.drawString(
                         camera.getRightBorder() +" | "+ camera.getBottomBorder(),
-                        (int) (viewportPosX + scaleX * camera.getGamePosX() / Block.DIMENSION
-                        + scaleX*camera.get2DWidth() / Block.DIMENSION),
-                        (int) (viewportPosY + scaleY * camera.getGamePosY() / Block.DIM2
-                        + scaleY *2*(controller.getPlayer().getCoords().getZ() * Block.DIM2)/ (float) (Block.DIMENSION)
-                        + scaleY*camera.get2DHeight() / Block.DIM2),
+                        (int) (viewportPosX + scaleX * camera.getGamePosX() / Block.SCREEN_DEPTH
+                        + scaleX*camera.get2DWidth() / Block.SCREEN_DEPTH),
+                        (int) (viewportPosY + scaleY * camera.getGamePosY() / Block.SCREEN_DEPTH2
+                        + scaleY *2*(controller.getPlayer().getCoords().getZ() * Block.SCREEN_DEPTH2)/ (float) (Block.SCREEN_DEPTH)
+                        + scaleY*camera.get2DHeight() / Block.SCREEN_DEPTH2),
                         Color.BLACK
                     );
 

com/BombingGames/EngineCore/WECamera.java
@@ -100,10 +100,10 @@ public class WECamera extends Camera {
     public void update() {       
         //refrehs the camera's position in the game world
         if (focusCoordinates != null) {
-            outputPosX = focusCoordinates.getBlock().get2DPosX(focusCoordinates) - get2DWidth() / 2 - AbstractGameObject.DIM2;
+            outputPosX = focusCoordinates.getBlock().get2DPosX(focusCoordinates) - get2DWidth() / 2 - AbstractGameObject.SCREEN_DEPTH2;
             outputPosY = focusCoordinates.getBlock().get2DPosY(focusCoordinates) - get2DHeight() / 2;
         } else if (focusentity != null ){
-            outputPosX = focusentity.get2DPosX(null) - get2DWidth()/2 + AbstractGameObject.DIM2;            
+            outputPosX = focusentity.get2DPosX(null) - get2DWidth()/2 + AbstractGameObject.SCREEN_DEPTH2;            
             outputPosY = focusentity.get2DPosY(null) - get2DHeight()/2 ;
         }
         
@@ -209,7 +209,7 @@ public class WECamera extends Camera {
      * @return 
      */
     public int getLeftBorder(){
-        leftborder = outputPosX / AbstractGameObject.DIMENSION - 1;
+        leftborder = outputPosX / AbstractGameObject.SCREEN_WIDTH - 1;
         if (leftborder < 0) leftborder= 0;
         
         return leftborder;
@@ -220,7 +220,7 @@ public class WECamera extends Camera {
      * @return
      */
     public int getRightBorder(){
-        rightborder = (outputPosX + get2DWidth()) / AbstractGameObject.DIMENSION + 1;
+        rightborder = (outputPosX + get2DWidth()) / AbstractGameObject.SCREEN_WIDTH + 1;
         if (rightborder >= Map.getBlocksX()) rightborder = Map.getBlocksX()-1;
 
         return rightborder;
@@ -231,7 +231,7 @@ public class WECamera extends Camera {
      * @return measured in blocks
      */
     public int getTopBorder(){    
-        topborder = outputPosY / AbstractGameObject.DIM4 - 3;
+        topborder = outputPosY / AbstractGameObject.SCREEN_DEPTH2 - 3;
         if (topborder < 0) topborder= 0;
         
         return topborder;
@@ -242,7 +242,7 @@ public class WECamera extends Camera {
      * @return measured in blocks
      */
     public int getBottomBorder(){
-        bottomborder = (outputPosY+get2DHeight()) / AbstractGameObject.DIM4 + Map.getBlocksZ()*2;
+        bottomborder = (outputPosY+get2DHeight()) / AbstractGameObject.SCREEN_DEPTH2 + Map.getBlocksZ()*2;
         if (bottomborder >= Map.getBlocksY()) bottomborder = Map.getBlocksY()-1;
         return bottomborder;
     }

com/BombingGames/Game/Gameobjects/AbstractCharacter.java
@@ -10,14 +10,14 @@ import com.badlogic.gdx.audio.Sound;
  * @author Benedikt
  */
 public abstract class AbstractCharacter extends AbstractEntity {
-   private final int COLISSIONRADIUS = AbstractGameObject.DIM4;
+   private final int COLISSIONRADIUS = SCREEN_DEPTH2;
    private final int SPRITESPERDIR;
       
    private float[] dir = {1, 0, 0};
    private String controls = "NPC";
 
-   /** Set value how fast the character brakes or slides. 1 is "immediately". The hgiher the value, teh more "slide". Value >1**/
-   private final int smoothBreaks = 80;
+   /** Set value how fast the character brakes or slides. 1 is "immediately". The higher the value, the more "slide". Value >1**/
+   private final int smoothBreaks = 1;
       
    /**provides a factor for the vector*/
    private float speed;
@@ -64,16 +64,16 @@ public abstract class AbstractCharacter extends AbstractEntity {
      */
     public void walk(boolean up, boolean down, boolean left, boolean right, float walkingspeed) {
         if (up || down || left || right){
-        speed = walkingspeed;
+            speed = walkingspeed;
 
-        //update the movement vector
-        dir[0] = 0;
-        dir[1] = 0;
+            //update the movement vector
+            dir[0] = 0;
+            dir[1] = 0;
 
-        if (up)    dir[1] = -1;
-        if (down)  dir[1] = 1;
-        if (left)  dir[0] = -1;
-        if (right) dir[0] = 1;
+            if (up)    dir[1] = -1;
+            if (down)  dir[1] = 1;
+            if (left)  dir[0] = -1;
+            if (right) dir[0] = 1;
         }
    }
     
@@ -84,8 +84,8 @@ public abstract class AbstractCharacter extends AbstractEntity {
      */
     private void makeCoordinateStep(int x, int y){
         //mirror the position around the center
-        setPositionX(getPositionX() -x*Block.DIM2);
-        setPositionY(getPositionY() -y*Block.DIM2);
+        setPositionX(getPositionX() -x*Block.SCREEN_WIDTH2);
+        setPositionY(getPositionY() -y*Block.SCREEN_DEPTH);
 
         setCoords(getCoords().addVector(0, y, 0));
         if (x < 0 && getCoords().getRelY() % 2 == 1) setCoords(getCoords().addVector(-1, 0, 0));
@@ -106,10 +106,6 @@ public abstract class AbstractCharacter extends AbstractEntity {
                 dir[0] /= vectorLenght;
                 dir[1] /= vectorLenght;
             }
-
-            //slow walking down
-            if (speed > 0) speed -= speed*delta/(float) smoothBreaks;
-            if (speed < 0) speed = 0;
 
             float oldPositionX = getPositionX();
             float oldPositionY = getPositionY();
@@ -236,6 +232,9 @@ public abstract class AbstractCharacter extends AbstractEntity {
                     fallingSoundPlaying = false;
                 }
             }
+            //slow walking down
+            if (speed > 0) speed -= speed*delta/(float) smoothBreaks;
+            if (speed < 0) speed = 0;
         }
     }
     
@@ -250,46 +249,46 @@ public abstract class AbstractCharacter extends AbstractEntity {
         
         //check for movement in x
         //top corner
-        int neighbourNumber = Block.getSideID(newx, newy - COLISSIONRADIUS); 
+        int neighbourNumber = Coordinate.getNeighbourSide(newx, newy - COLISSIONRADIUS); 
         if (neighbourNumber != 8 && Controller.getNeighbourBlock(getCoords(), neighbourNumber).isObstacle())
             validmovement = true;
         //bottom corner
-        neighbourNumber = Block.getSideID(newx, newy + COLISSIONRADIUS); 
+        neighbourNumber = Coordinate.getNeighbourSide(newx, newy + COLISSIONRADIUS); 
         if (neighbourNumber != 8 && Controller.getNeighbourBlock(getCoords(), neighbourNumber).isObstacle())
             validmovement = true;
 
         //find out the direction of the movement
         if (oldx - newx > 0) {
             //check left corner
-            neighbourNumber = Block.getSideID(newx - COLISSIONRADIUS, newy);
+            neighbourNumber = Coordinate.getNeighbourSide(newx - COLISSIONRADIUS, newy);
             if (neighbourNumber != 8 && Controller.getNeighbourBlock(getCoords(), neighbourNumber).isObstacle())
                 validmovement = true;
         } else {
             //check right corner
-            neighbourNumber = Block.getSideID(newx + COLISSIONRADIUS, newy);
+            neighbourNumber = Coordinate.getNeighbourSide(newx + COLISSIONRADIUS, newy);
             if (neighbourNumber != 8 && Controller.getNeighbourBlock(getCoords(), neighbourNumber).isObstacle())
                 validmovement = true;
         }
 
         //check for movement in y
         //left corner
-        neighbourNumber = Block.getSideID(newx - COLISSIONRADIUS, newy); 
+        neighbourNumber = Coordinate.getNeighbourSide(newx - COLISSIONRADIUS, newy); 
         if (neighbourNumber != 8 && Controller.getNeighbourBlock(getCoords(), neighbourNumber).isObstacle())
             validmovement = true;
 
         //right corner
-        neighbourNumber = Block.getSideID(newx + COLISSIONRADIUS, newy); 
+        neighbourNumber = Coordinate.getNeighbourSide(newx + COLISSIONRADIUS, newy); 
         if (neighbourNumber != 8 && Controller.getNeighbourBlock(getCoords(), neighbourNumber).isObstacle())
             validmovement = true; 
 
         if (oldy - newy > 0) {
             //check top corner
-            neighbourNumber = Block.getSideID(newx, newy - COLISSIONRADIUS);
+            neighbourNumber = Coordinate.getNeighbourSide(newx, newy - COLISSIONRADIUS);
             if (neighbourNumber != 8 && Controller.getNeighbourBlock(getCoords(), neighbourNumber).isObstacle())
                 validmovement = true;
         } else {
             //check bottom corner
-            neighbourNumber = Block.getSideID(newx, newy + COLISSIONRADIUS);
+            neighbourNumber = Coordinate.getNeighbourSide(newx, newy + COLISSIONRADIUS);
             if (neighbourNumber != 8 && Controller.getNeighbourBlock(getCoords(), neighbourNumber).isObstacle())
                 validmovement = true;
         }

commit db86c60a27726bc9c045927a15125fd8f1db9baa

Fixed bug introduced by 8abd0ffb26d57e2d19f8cbed260de24bcf6b74a1

com/BombingGames/EngineCore/View.java
@@ -165,7 +165,7 @@ public class View {
         //find out where the click went
         Coordinate coords = new Coordinate(
                        x / Block.SCREEN_WIDTH -1,
-                       2*y / Block.SCREEN_DEPTH*2 -1,
+                       2*y / (Block.SCREEN_DEPTH*2) -1,
                        Map.getBlocksZ()-1,
                         true
         );