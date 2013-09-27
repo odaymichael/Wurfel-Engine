c6c279fca96250597b2e5cbc0e13839191e8696d
ThTomate <remailer@abwesend.de>
2013-09-27 15:56:40 +0200

    made MainMenu faster portable

--- com/BombingGames/MainMenu/View.java
+++ com/BombingGames/MainMenu/View.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import org.lwjgl.opengl.GL11;
 
 
 /**
@@ -54,8 +53,8 @@ public class View {
      */
     public void render(Controller pController){
         //clear & set background to black
-        GL11.glClearColor( 0f, 0f, 0f, 1f );
-        GL11.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        Gdx.gl11.glClearColor( 0f, 0f, 0f, 1f );
+        Gdx.gl11.glClear(GL10.GL_COLOR_BUFFER_BIT);
         
         //update camera and set the projection matrix
         camera.update();
