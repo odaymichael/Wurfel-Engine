fa056f90d96320282ef340e63b9f47c2c2eee92a
Benedikt Vogler <e-mail@benediktvogler.de>
2013-09-29 14:59:59 +0200

    refactoring

--- com/BombingGames/EngineCore/Gameobjects/Sea.java
+++ com/BombingGames/EngineCore/Gameobjects/Sea.java
@@ -8,8 +8,8 @@ import com.BombingGames.EngineCore.Map.Map;
  * @author Benedikt Vogler
  */
 public class Sea extends Block implements IsSelfAware{
-    public static final int WAVE_AMPLITUDE = 50;
-    private static final float wavespeed = 1/600f; //the smaller the slower
+    public static final int WAVE_AMPLITUDE = AbstractGameObject.SCREEN_HEIGHT-10;
+    private static final float wavespeed = 1/700f; //the smaller the slower
     private static float currentX = 0;
     private final int wavesize = Map.getBlocksX()/7;
     
@@ -49,7 +49,7 @@ public class Sea extends Block implements IsSelfAware{
             startvalue +
             (float) (Math.sin(
                 (currentX-coords.getRelX()-coords.getRelY())
-                * Math.PI/wavesize
+                * Math.PI/waveWidth
                 )*WAVE_AMPLITUDE
             )
         );
@@ -62,7 +62,5 @@ public class Sea extends Block implements IsSelfAware{
     public static void staticUpdate(float delta){
         currentX += delta*wavespeed;
     }
-    
-    
-    
+
 }
